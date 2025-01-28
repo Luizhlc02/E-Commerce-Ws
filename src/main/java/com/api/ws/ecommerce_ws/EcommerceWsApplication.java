c8befd547170e0429a516e8bfe192a2438b4d91c
Luizhlc02 <luizhlc02@gmail.com>
2025-01-27 22:08:17 -0300

    first commit

---
+++ src/main/java/com/api/ws/ecommerce_ws/EcommerceWsApplication.java
@@ -0,0 +1,13 @@
+package com.api.ws.ecommerce_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ws/ecommerce_ws/EcommerceWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ws.ecommerce_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
